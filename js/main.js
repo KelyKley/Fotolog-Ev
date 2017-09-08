be273443ba6a277fff4fbe565c20ee2be3b4631f
kelyanamuro <kelyanamurohuamani@gmail.com>
2017-09-08 10:28:03 -0500

    Ejercicio evaluado

--- js/main.js
+++ js/main.js
@@ -29,7 +29,7 @@ function saveData(){
 
 		div_master.appendChild(div_hijo);
 
-		//limpiar input de nombre y comentario
+		//limpiar input de nombre y comentario 
 		document.getElementById("name").value = "";
 		document.getElementById("clave").value = "";
 
@@ -47,26 +47,3 @@ function clearData(){
 function eliminar(){
 	localStorage.clear(); saveData();
 }
-
-
-/*function saveData(){
-	var name = document.getElementById("name").value;
-	var clave = document.getElementById("clave").value;
-	sessionStorage.setItem(name, clave);
-}
-
-function recoverData(){
-	for (var i = 0; i<sessionStorage.length; i++){
-		var name = sessionStorage.key(i);
-		var clave = sessionStorage.getItem(name);
-		document.getElementById("data").innerHTML = "<div>" + "Nombre : "+name+ "<br/> Clave : "+ clave + "</div>";
-		}
-	}
-
-function clearData(){
-	document.getElementById("data").innerHTML = "Limpiada vista. Los datos permanecen";
-}
-
-function borrarTodo(){
-	sessionStorage.clear(); recoverData();
-}*/
